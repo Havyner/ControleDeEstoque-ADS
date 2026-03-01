8aa393c76b44ba139cc74930861f6dd379961797
Havyner <havy012_@hotmail.com>
2026-03-01 20:17:37 -0300

    Implementacao entrada de produto

--- ControleDeEstoque.py
+++ ControleDeEstoque.py
@@ -8,10 +8,17 @@ Desenvolver um sistema simples de controle de estoque em Python,
 com funcionalidade de entrada e saída de produtos, Conforme a História de Usuário fornecida.
 '''
 
+#Biblioteca para registrar a data de entrada
+from datetime import datetime
+
+#Estoque vai começar vazio e será inclementado
 estoque = {}
+#Lista para registrar a movimentações
+movimentacaoList = []
+
 
 def cadastrar_produto():
-    nome_produto = input("Digite o nome do produto: ").strip() #Usando a função strip para remover espaços e se adequar
+    nome_produto = input("Digite o nome do produto: ").lower() #Usando a função lower para deixar tudo minusculo
 
     #Verificando se o produto já esta cadastrado
     if nome_produto in estoque:
@@ -22,8 +29,38 @@ def cadastrar_produto():
         print("\nProduto cadastrado com sucesso!")
 
 def entrada_produto():
-    print("ok")
+    nome_produto = input("\nInforme o nome do produto: ").lower()
 
+    #verificando se possui o produto em estoque
+    if nome_produto not in estoque:
+        print("Produto não encontrado!")
+        return
+    
+    try:
+        qnt = int(input("Informe a quantidade de entrada do produto: "))
+        #Verificar se a quantidade é válida
+        if qnt <= 0:
+            print("\nQuantidade deve ser maior que zero")
+            return
+    except ValueError:
+        print("\nDigite um número válido!")
+        return
+    #Registrando a entrada do produto
+    data = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    funcionario = input("Digite o nome do Funcionário responsável: ").lower()
+
+    #Atualizando estoque
+    estoque[nome_produto] += qnt
+
+    movimentacaoList.append({
+        "Tipo" : "Entrada",
+        "produto" : nome_produto,
+        "Quantidade" : qnt,
+        "Data" : data,
+        "Responsável" : funcionario   
+    })
+
+    print("Registro realizado com sucesso!")
 
 def saida_produto():
     print("ok")
